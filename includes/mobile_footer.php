8583e202a390d62b8f0c1843fb43a2b09be02f80
Isabel <0247isanchez@e-itaca.es>
2025-03-08 17:28:00 +0100

    Modificacion en mobile_footer.php

--- includes/mobile_footer.php
+++ includes/mobile_footer.php
@@ -3,25 +3,25 @@
             <div class="mobile-block block-menu-main">
                 <a class="menu-bar menu-toggle btn-toggle" data-object="open-mobile-menu" href="javascript:void(0)">
                     <span class="fa fa-bars"></span>
-                    <span class="text">Menu</span>
+                    <span class="text">Menu de Principal</span> <!--Cambio de Isabel-->
                 </a>
             </div>
             <div class="mobile-block block-sidebar">
                 <a class="menu-bar filter-toggle btn-toggle" data-object="open-mobile-filter" href="javascript:void(0)">
                     <i class="fa fa-sliders" aria-hidden="true"></i>
-                    <span class="text">Sidebar</span>
+                    <span class="text">Barra lateral</span> <!--Cambio de Isabel-->
                 </a>
             </div>
             <div class="mobile-block block-minicart">
                 <a class="link-to-cart" href="#">
                     <span class="fa fa-shopping-bag" aria-hidden="true"></span>
-                    <span class="text">Cart</span>
+                    <span class="text">Carrito de Compras</span> <!--Cambio de Isabel-->
                 </a>
             </div>
             <div class="mobile-block block-global">
                 <a class="menu-bar myaccount-toggle btn-toggle" data-object="global-panel-opened" href="javascript:void(0)">
                     <span class="fa fa-globe"></span>
-                    <span class="text">Global</span>
+                    <span class="text">Global</span> <!--Cambio de Isabel-->
                 </a>
             </div>
         </div>
